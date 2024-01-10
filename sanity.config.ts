e028421424c25d49e3d4186264133c6230fd5630
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-10 23:40:05 +0100

    Initial commit

---
+++ sanity.config.ts
@@ -0,0 +1,36 @@
+/**
+ * This configuration is used to for the Sanity Studio that’s mounted on the `\app\studio\[[...index]]\page.tsx` route
+ */
+import { locate } from "@/sanity/presentation/locate";
+
+import { presentationTool } from "sanity/presentation";
+
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+
+// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import { apiVersion, dataset, projectId } from "./sanity/env";
+import { schema } from "./sanity/schema";
+
+export default defineConfig({
+  basePath: "/studio",
+  projectId,
+  dataset,
+  // Add and edit the content schema in the './sanity/schema' folder
+  schema,
+  plugins: [
+    presentationTool({
+      locate,
+      previewUrl: {
+        draftMode: {
+          enable: "/api/draft",
+        },
+      },
+    }),
+    structureTool(),
+    // Vision is a tool that lets you query your content with GROQ in the studio
+    // https://www.sanity.io/docs/the-vision-plugin
+    visionTool({ defaultApiVersion: apiVersion }),
+  ],
+});
