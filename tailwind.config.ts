4dfea92ffb6cf917125c1c788a27684689cfe8f7
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-12 00:09:17 +0100

    feat(tailwind): import several settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 const config: Config = {
   darkMode: "class",
@@ -8,14 +9,48 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       fontFamily: {
         sans: ["var(--font-blinker)", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        blue: {
+          DEFAULT: "#0B8DD8",
+          50: "#F8FCFF",
+          100: "#DBF1FD",
+          200: "#A0D9FA",
+          300: "#66C2F7",
+          400: "#2CABF4",
+          500: "#0B8DD8",
+          600: "#0974B1",
+          700: "#075A8A",
+          800: "#054164",
+          900: "#03283D",
+        },
+        orange: {
+          DEFAULT: "#FF8116",
+          50: "#FFFDFC",
+          100: "#FFEFE2",
+          200: "#FFD4AF",
+          300: "#FFB87C",
+          400: "#FF9D49",
+          500: "#FF8116",
+          600: "#E26800",
+          700: "#AF5000",
+          800: "#7C3900",
+          900: "#492200",
+        },
+      },
+      grayscale: {
+        50: "50%",
       },
     },
   },
