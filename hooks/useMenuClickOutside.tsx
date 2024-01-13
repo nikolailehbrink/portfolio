9361e1add3dbc6b9069bd42e7fe25dc24cf89e34
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-13 17:37:46 +0100

    feat: create useMenuClickOutside hook
    - Add event listener to the document if the menu is open
    - Close menu if click is outside of menu and is not triggered by the menu button

---
+++ hooks/useMenuClickOutside.tsx
@@ -0,0 +1,34 @@
+import type { RefObject } from "react";
+import { useEffect } from "react";
+
+export function useMenuClickOutside(
+  menuRef: RefObject<HTMLElement>,
+  buttonRef: RefObject<HTMLElement>,
+  onClickOutside: () => void,
+  isMenuOpen: boolean,
+) {
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      console.log(event.target);
+
+      if (
+        menuRef.current &&
+        !menuRef.current.contains(event.target as Node) &&
+        !(
+          event.target === buttonRef.current ||
+          buttonRef.current?.contains(event.target as Node)
+        )
+      ) {
+        onClickOutside();
+      }
+    }
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+    // Bind
+    return () => {
+      // dispose
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef, buttonRef, onClickOutside, isMenuOpen]);
+}
