ce140cfb5b4c7394938a7c5c0e575a99cdd515f3
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-14 23:33:44 +0100

    feat(sanity): create services schema and type

---
+++ sanity/schemas/services.ts
@@ -0,0 +1,43 @@
+import { defineField, defineType } from "sanity";
+
+export default defineType({
+  name: "services",
+  title: "Services",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+    defineField({
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: "alt",
+          type: "string",
+          title: "Alternativ Text",
+        },
+      ],
+    }),
+  ],
+});
