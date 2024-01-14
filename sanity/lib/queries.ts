2162175ddc1f1c1814033cebbf978fa17a63f8bb
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-14 23:33:58 +0100

    feat: add query for services

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -3,3 +3,5 @@ import { groq } from "next-sanity";
 export const POSTS_QUERY = groq`*[_type == "post" && defined(slug)]`;
 
 export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
+
+export const SERVICES_QUERY = groq`*[_type == "services" && defined(slug)]`;
