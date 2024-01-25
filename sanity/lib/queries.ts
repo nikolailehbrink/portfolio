fe7d705e0993882e6933709c3791710b9d34dca8
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-25 13:07:48 +0100

    feat(experience section): get content from sanity
    - Extract expertise grid to own component
    - Get sanity data for work experience

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -7,3 +7,5 @@ export const POST_QUERY = groq`*[_type == "post" && slug.current == $slug][0]`;
 export const SERVICES_QUERY = groq`*[_type == "service" && defined(slug)]`;
 
 export const PROJECTS_QUERY = groq`*[_type == "project" && defined(slug)]`;
+
+export const EXPERIENCES_QUERY = groq`*[_type == "experience"]`;
