fe391b6bcdf4e5bf828827cc8447eb9cef4ac02c
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-28 00:00:39 +0100

    feat: add twConfig utility
    This way the values of the tailwind config can be used in all js files.

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "@/tailwind.config";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -8,3 +10,5 @@ export function cn(...inputs: ClassValue[]) {
 export function isDev() {
   return process.env.NODE_ENV === "development";
 }
+
+export const twConfig = resolveConfig(tailwindConfig);
