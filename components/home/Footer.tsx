5d39e16a192a1f4eca435b0a5d3e1992a84745fd
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-28 20:45:31 +0100

    feat(Footer): add source code link

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -1,5 +1,13 @@
 import footerNavigation from "@/data/Home/FooterNavigation";
+import { Code } from "@phosphor-icons/react/dist/ssr";
 import Link from "next/link";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { twConfig } from "@/lib/utils";
 
 export default function Footer() {
   return (
@@ -9,9 +17,32 @@ export default function Footer() {
           &copy; {new Date().getFullYear()} | Nikolai Lehbrink - Web Development
           & Design
         </span>
-        {footerNavigation.length > 0 && (
-          <menu className="flex gap-2">
-            {footerNavigation.map((item, index) => (
+        <menu className="flex gap-2">
+          <li>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger>
+                  <Link
+                    target="_blank"
+                    href={"https://github.com/nikolailehbrink/website"}
+                  >
+                    <Code
+                      className="inline"
+                      size={twConfig.theme.spacing[6]}
+                      weight="duotone"
+                    />
+                  </Link>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Source Code</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+
+            <span className="ml-2">|</span>
+          </li>
+          {footerNavigation.length > 0 &&
+            footerNavigation.map((item, index) => (
               <li key={item.link}>
                 <Link
                   className="hover:underline hover:underline-offset-2"
@@ -24,8 +55,7 @@ export default function Footer() {
                 )}
               </li>
             ))}
-          </menu>
-        )}
+        </menu>
       </div>
     </footer>
   );
