078b2d8e7fe53715947f54a75a4df1e33e799e51
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-29 17:37:22 +0100

    style(Code block): update border-radius

--- components/Code.tsx
+++ components/Code.tsx
@@ -17,7 +17,7 @@ export default async function Code({
 }) {
   const html = await codeToHtml(code, { lang: language, theme: "nord" });
   return (
-    <div className="overflow-hidden rounded-xl bg-gradient-to-r from-blue-200 to-blue-400 p-4 !pr-0 prose-pre:my-0 prose-pre:rounded-none md:p-8 lg:p-12">
+    <div className="overflow-hidden rounded-lg bg-gradient-to-r from-blue-200 to-blue-400 p-4 !pr-0 prose-pre:my-0 prose-pre:rounded-none md:p-8 lg:p-12">
       <div className="overflow-hidden rounded-s-lg">
         <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4 text-sm">
           <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-border bg-neutral-800 px-4 py-2 ">
