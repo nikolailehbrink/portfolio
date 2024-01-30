517edb5e269f9ba45eccebc2171f280146bf1746
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-30 19:18:56 +0100

    feat: update post design

--- components/blog/Post.tsx
+++ components/blog/Post.tsx
@@ -5,24 +5,49 @@ import PostImageComponent from "../PostImageComponent";
 import type { SanityPost } from "@/types/sanity/sanityPost";
 import { useNextSanityImage } from "next-sanity-image";
 import { client } from "@/sanity/lib/client";
+import GoBackButton from "../GoBackButton";
 
 export default function Post({ post }: { post: SanityPost }) {
-  const { title, mainImage, body } = post;
+  const { title, mainImage, body, _createdAt, author, categories } = post;
+  const date = new Date(_createdAt);
+
+  console.log(post);
 
   const { height, src, width } = useNextSanityImage(client, mainImage, {
     imageBuilder: (image) => image.fit("max").width(1920).height(1080),
   });
 
   return (
-    <article className="container space-y-8 py-8">
-      {title ? (
-        <h1 className="mx-auto max-w-3xl text-2xl font-bold sm:text-3xl lg:text-center lg:text-5xl">
-          {title}
-        </h1>
-      ) : null}
+    <article className="container space-y-8 py-8 lg:pt-16">
+      <GoBackButton />
+      <div className="flex flex-col items-center justify-center gap-4">
+        <div className="flex gap-2">
+          {categories.map((category) => (
+            <div key={category._id} className="badge badge-orange">
+              {category.title}
+            </div>
+          ))}
+        </div>
+        {title ? (
+          <h1 className="mx-auto max-w-3xl text-2xl font-bold sm:text-3xl lg:text-center lg:text-5xl">
+            {title}
+          </h1>
+        ) : null}
+        <div className="flex gap-2">
+          <span>
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            }).format(date)}
+          </span>
+          <span>|</span>
+          <span>{author.name}</span>
+        </div>
+      </div>
       {mainImage ? (
         <Image
-          className="aspect-video w-full rounded-lg object-cover"
+          className="relative z-10 aspect-video w-full rounded-lg object-cover"
           src={src}
           width={width}
           height={height}
