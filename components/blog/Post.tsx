02fc3efbda52512a0ce9a0345c3227ae247586f7
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-31 12:40:16 +0100

    feat(blog): update single post page

--- components/blog/Post.tsx
+++ components/blog/Post.tsx
@@ -6,21 +6,26 @@ import type { SanityPost } from "@/types/sanity/sanityPost";
 import { useNextSanityImage } from "next-sanity-image";
 import { client } from "@/sanity/lib/client";
 import GoBackButton from "../GoBackButton";
+import { useReadingTime } from "@/hooks/useReadingTime";
+import { getSanityBodyText } from "@/sanity/helpers";
 
 export default function Post({ post }: { post: SanityPost }) {
-  const { title, mainImage, body, _createdAt, author, categories } = post;
-  const date = new Date(_createdAt);
-
-  console.log(post);
+  const { title, mainImage, body, _createdAt, categories } = post;
 
   const { height, src, width } = useNextSanityImage(client, mainImage, {
     imageBuilder: (image) => image.fit("max").width(1920).height(1080),
   });
 
+  const postCreated = new Date(_createdAt);
+
+  const postText = getSanityBodyText(body);
+
+  const { minutesToRead } = useReadingTime(postText);
+
   return (
-    <article className="container space-y-8 py-8 lg:pt-16">
+    <article className="container my-8 space-y-8">
       <GoBackButton className="lg:sticky lg:top-24" />
-      <div className="flex flex-col items-center justify-center gap-4">
+      <div className="flex flex-col justify-center gap-4 sm:items-center sm:text-center">
         <div className="flex gap-2">
           {categories.map((category) => (
             <div key={category._id} className="badge badge-orange">
@@ -29,20 +34,18 @@ export default function Post({ post }: { post: SanityPost }) {
           ))}
         </div>
         {title ? (
-          <h1 className="mx-auto max-w-3xl text-2xl font-bold sm:text-3xl lg:text-center lg:text-5xl">
-            {title}
-          </h1>
+          <h1 className="max-w-3xl  text-3xl font-bold lg:text-5xl">{title}</h1>
         ) : null}
-        <div className="flex gap-2">
-          <span>
+        <div className="flex gap-2 text-neutral-400">
+          <time dateTime={postCreated.toISOString()}>
             {new Intl.DateTimeFormat("en-US", {
               year: "numeric",
-              month: "long",
+              month: "short",
               day: "numeric",
-            }).format(date)}
-          </span>
+            }).format(postCreated)}
+          </time>
           <span>|</span>
-          <span>{author.name}</span>
+          <p>{minutesToRead}m read</p>
         </div>
       </div>
       {mainImage ? (
@@ -51,7 +54,7 @@ export default function Post({ post }: { post: SanityPost }) {
           src={src}
           width={width}
           height={height}
-          alt={mainImage.alt || ""}
+          alt={mainImage.alt || "Post image"}
           priority
           sizes="
           (max-width: 768px) 95vw,
@@ -61,11 +64,9 @@ export default function Post({ post }: { post: SanityPost }) {
       ) : null}
       {body ? (
         <section className="prose prose-lg mx-auto dark:prose-invert">
-          {" "}
           <PortableText
             value={body}
             components={{
-              // ...
               types: {
                 code: ({ value }) => <CodeBlock {...value} />,
                 image: PostImageComponent,
