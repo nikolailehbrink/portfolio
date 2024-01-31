9980e571660d27d0f604b0e2fc9e32074f657772
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-01-31 12:39:46 +0100

    feat(Blog): add header section

--- components/blog/Posts.tsx
+++ components/blog/Posts.tsx
@@ -11,6 +11,15 @@ export default function Posts({ posts }: { posts: SanityPost[] }) {
   return (
     <main className="container mx-auto my-8 grid grid-cols-1 space-y-8">
       <GoBackButton />
+      <header className="prose mx-auto dark:prose-invert md:text-center">
+        <h1 className="my-3 text-5xl">Blog</h1>
+        <p>
+          Thanks for visiting my blog. Here I share my thoughts on web
+          development, design and other topics that interest me. If you have any
+          questions, comments or post ideas please feel free to{" "}
+          <Link href="/#contact">reach out to me.</Link>
+        </p>
+      </header>
       {firstPost && <PostTeaser post={firstPost} />}
       {otherPosts?.length > 0 ? (
         <div
