997690403d180d453cd649a79a73eb1615008978
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-01 11:23:52 +0100

    feat: add blog page loading ui

---
+++ components/blog/PostTeaserSkeleton.tsx
@@ -0,0 +1,45 @@
+import { cn } from "@/lib/utils";
+import { Skeleton } from "../ui/skeleton";
+
+export default function PostTeaserSkeleton({
+  className,
+}: {
+  className?: string;
+}) {
+  return (
+    <div className={cn("@container", className)}>
+      <div className="relative grid grid-cols-1 items-center gap-4 @4xl:grid-cols-2 @4xl:gap-8">
+        <div className="relative overflow-hidden rounded-lg">
+          <Skeleton className="aspect-video w-full" />
+          <div className="absolute inset-0 top-1/2 bg-gradient-to-t from-neutral-950 to-transparent"></div>
+          <div className="absolute bottom-4 right-4 flex gap-2">
+            <Skeleton className="h-8 w-16 rounded-lg"></Skeleton>
+          </div>
+        </div>
+        <div className="flex flex-col items-start justify-center gap-4">
+          <Skeleton className="hidden h-7 w-20 @4xl:flex" />
+          <div className="flex gap-2 text-sm text-neutral-400">
+            <Skeleton className="h-4 w-14"></Skeleton>
+            <Skeleton className="h-4 w-14"></Skeleton>
+            <Skeleton className="h-4 w-14"></Skeleton>
+          </div>
+          <div className="w-full space-y-2">
+            <Skeleton className="h-7 w-1/2"></Skeleton>
+            <Skeleton className="h-7 w-2/3"></Skeleton>
+          </div>
+          <div className="w-full space-y-2">
+            <Skeleton className="h-4 w-3/4" />
+            <Skeleton className="h-4 w-2/3" />
+          </div>
+          <div className="hidden w-full items-center gap-2 @4xl:flex">
+            <Skeleton className="h-10 w-10 flex-shrink-0 rounded-full" />
+            <div className="w-full space-y-2">
+              <Skeleton className="h-4 w-40"></Skeleton>
+              <Skeleton className="h-4 w-48"></Skeleton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
