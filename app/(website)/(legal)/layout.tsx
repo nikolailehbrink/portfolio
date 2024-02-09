28dd67a39a53bdbac706fd0b53ba8728f282cffd
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-09 18:09:01 +0100

    feat: update legal texts

--- app/(website)/(legal)/layout.tsx
+++ app/(website)/(legal)/layout.tsx
@@ -3,9 +3,9 @@ import type { ReactNode } from "react";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="container">
-      <article className="prose prose-neutral mx-auto my-8 dark:prose-invert prose-h1:mb-2">
+      <section className="prose prose-neutral mx-auto my-8 dark:prose-invert prose-h1:mb-2">
         {children}
-      </article>
+      </section>
     </div>
   );
 }
