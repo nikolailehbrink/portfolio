4264fba7c46dab600e04aa35694c3f3445a62ebd
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-13 22:02:13 +0100

    feat: add text animation for first chat message

--- components/chat/ChatMessages.tsx
+++ components/chat/ChatMessages.tsx
@@ -2,6 +2,7 @@ import type { UseChatHelpers } from "ai/react";
 import SpinnerAlt from "@/assets/icons/unicons/spinner-alt.svg";
 import { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
+import { gsap, useGSAP } from "@/lib/gsap";
 
 export default function ChatMessages({
   messages,
@@ -17,6 +18,8 @@ export default function ChatMessages({
     }
   };
 
+  const messageRef = useRef<HTMLDivElement>(null);
+
   const isLastMessageFromAssistant =
     messages.length > 0 && lastMessage?.role !== "user";
   const isPending = isLoading && !isLastMessageFromAssistant;
@@ -25,14 +28,22 @@ export default function ChatMessages({
     scrollToBottom();
   }, [messages.length, lastMessage]);
 
+  useGSAP(
+    () => {
+      gsap.to("p", {
+        text: "Hi! This is Nikolai in a digital form. I wanted to do a kind of different about me section and I thought it would be cool if you can ask anything you want to know about me. So, go ahead and ask me anything! This AI will respond with data from me which it is trained on!",
+        duration: 8,
+      });
+    },
+    { scope: messageRef },
+  );
+
   return (
-    <div className="my-8 flex-1 overflow-auto rounded-xl bg-neutral-950 scrollbar-thin">
-      <div className="space-y-4 p-8" ref={scrollableChatContainerRef}>
-        <ChatMessage
-          content="Hi, ich bin Nikolai und stehe dir gerne zur Verfügung. Wie kann ich dir helfen?"
-          role="assistant"
-          id="12344"
-        />
+    <div className="mx-auto my-8 w-full max-w-screen-lg flex-1 overflow-auto rounded-xl scrollbar-thin sm:bg-neutral-950">
+      <div className="space-y-4 sm:p-8" ref={scrollableChatContainerRef}>
+        <div ref={messageRef}>
+          <ChatMessage content="Hi!" role="assistant" id="12344" />
+        </div>
         {messages.map((m) => (
           <ChatMessage key={m.id} {...m} />
         ))}
--- lib/gsap.ts
+++ lib/gsap.ts
@@ -1,11 +1,14 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
+
 import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 
 gsap.defaults({
   duration: 1,
 });
 
-export { gsap, useGSAP, ScrollTrigger };
+export { gsap, useGSAP };
