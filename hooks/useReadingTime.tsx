51952eb48c039053ce1cd05fe9b3132343ed3fc0
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-17 19:33:08 +0100

    feat(useReadingTime): implement seconds to read variable

--- hooks/useReadingTime.tsx
+++ hooks/useReadingTime.tsx
@@ -2,7 +2,8 @@ export function useReadingTime(text: string) {
   const words = text.split(/\s+/).length;
   const wpm = 200;
   const minutesToRead = Math.ceil(words / wpm);
+  const secondsToRead = Math.ceil(words / (wpm / 60));
   const characters = text.length;
 
-  return { words, minutesToRead, characters };
+  return { words, minutesToRead, characters, secondsToRead };
 }
