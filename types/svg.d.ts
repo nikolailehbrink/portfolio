b0a8300436a7a9bd4e7870ce1ab2acc178be5eb5
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-22 09:15:27 +0100

    chore: add svg type

--- types/svg.d.ts
+++ types/svg.d.ts
@@ -2,3 +2,7 @@ declare module "*.svg?url" {
   const src: string;
   export default src;
 }
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
