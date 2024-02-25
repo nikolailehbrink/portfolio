1dde7d11a744486386e2ac42c381f724577c6ef7
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-02-25 17:46:05 +0100

    fix: remove typed routes because of type errors

--- next.config.js
+++ next.config.js
@@ -23,7 +23,6 @@ const nextConfig = {
       "/*": ["./cache/**/*"],
     },
     taint: true,
-    typedRoutes: true,
   },
   webpack(config) {
     // Tokenizer https://www.npmjs.com/package/tiktoken#nextjs
