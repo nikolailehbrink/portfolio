a2f77568f4f023a9a75bc3f3612a1a136120fd13
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-10 08:59:21 +0200

    style: update Navbar design

--- components/global/Navbar/index.tsx
+++ components/global/Navbar/index.tsx
@@ -36,46 +36,44 @@ export function Navbar() {
     useMenuClickOutside();
 
   const pathname = usePathname();
-
   const isHome = pathname === "/";
-
-  console.log("Again");
-
   return (
-    <header className="container sticky top-0 z-50 flex max-lg:justify-start lg:duration-1000 lg:animate-in lg:fade-in lg:slide-in-from-top-28">
-      <MenuButton
-        ref={menuButtonRef}
-        showMenu={showMenu}
-        setShowMenu={setShowMenu}
-      />
-      <nav
-        ref={menuRef}
-        className={cn(
-          `flex max-lg:rounded-lg max-lg:border-2 border-neutral-700 max-lg:bg-neutral-950 bg-neutral-900 p-2 font-bold max-lg:fixed max-lg:right-0 max-lg:top-20 max-lg:mr-4 max-lg:flex-col max-lg:items-center max-lg:gap-6 max-lg:p-8 max-lg:transition-transform lg:w-full`,
-          !showMenu && "max-lg:translate-x-[calc(100%_+_1rem)]"
-        )}
-      >
-        <div className="relative flex flex-1">
-          <Link
-            href={isHome ? "#top" : "/"}
-            aria-label={isHome ? "Scroll to top" : "Go to home page"}
-          >
-            <Logo className="size-12 rounded-full" />
-          </Link>
-        </div>
-        <menu
-          id="navigation"
-          role="menu"
-          aria-labelledby="menubutton"
-          className="flex justify-center gap-4 tracking-wide max-lg:flex-col lg:items-center"
+    <div className="sticky top-0 z-50 lg:bg-neutral-900/80 lg:backdrop-blur-2xl lg:py-3">
+      <header className="container flex max-lg:justify-start">
+        <MenuButton
+          ref={menuButtonRef}
+          showMenu={showMenu}
+          setShowMenu={setShowMenu}
+        />
+        <nav
+          ref={menuRef}
+          className={cn(
+            `flex max-lg:rounded-lg max-lg:border-2 border-neutral-700 max-lg:bg-neutral-950 font-bold max-lg:fixed max-lg:right-0 max-lg:top-20 max-lg:mr-4 max-lg:flex-col max-lg:items-center max-lg:gap-6 max-lg:p-8 max-lg:transition-transform lg:w-full`,
+            !showMenu && "max-lg:translate-x-[calc(100%_+_1rem)]"
+          )}
         >
-          {navigationItems.map(({ link, title, icon }) => (
-            <li role="none" key={link}>
-              <NavigationLink link={link} title={title} icon={icon} />
-            </li>
-          ))}
-        </menu>
-      </nav>
-    </header>
+          <div className="relative flex flex-1">
+            <Link
+              href={isHome ? "#top" : "/"}
+              aria-label={isHome ? "Scroll to top" : "Go to home page"}
+            >
+              <Logo className="size-12 rounded-full" />
+            </Link>
+          </div>
+          <menu
+            id="navigation"
+            role="menu"
+            aria-labelledby="menubutton"
+            className="flex justify-center gap-4 tracking-wide max-lg:flex-col lg:items-center"
+          >
+            {navigationItems.map(({ link, title, icon }) => (
+              <li role="none" key={link}>
+                <NavigationLink link={link} title={title} icon={icon} />
+              </li>
+            ))}
+          </menu>
+        </nav>
+      </header>
+    </div>
   );
 }
