7186d6f9fbd2a26af6d880977a6ad33e8475351b
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-16 12:18:20 +0200

    feat(CodeBlock): remove initial filename

--- components/shared/CodeBlock.tsx
+++ components/shared/CodeBlock.tsx
@@ -14,7 +14,7 @@ type Props = {
 export default async function CodeBlock({
   code,
   language = "typescript",
-  filename = "nikolailehbr.ink",
+  filename,
   highlightedLines,
   removedLines,
   addedLines,
@@ -31,9 +31,11 @@ export default async function CodeBlock({
     <div className="not-prose overflow-hidden rounded-lg bg-gradient-to-br from-sky-300 to-sky-500 p-4 !pr-0 md:p-8 lg:p-8 [&>pre]:rounded-none">
       <div className="overflow-hidden rounded-s-lg">
         <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4">
-          <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-border bg-neutral-800 px-3 py-1 text-sm text-neutral-400">
-            {filename}
-          </span>
+          {filename && (
+            <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-border bg-neutral-800 px-3 py-1 text-sm text-neutral-400">
+              {filename}
+            </span>
+          )}
           <CopyToClipboard text={code} />
         </div>
         <div
