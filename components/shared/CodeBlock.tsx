4ce699b4424bff5996e05a4ca8065e9985ecaabb
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-18 23:18:40 +0200

    style: move copy button to the end if no filename exists

--- components/shared/CodeBlock.tsx
+++ components/shared/CodeBlock.tsx
@@ -1,4 +1,5 @@
 import { highlightCode, transformCode } from "@/lib/shiki";
+import { cn } from "@/lib/utils";
 import type { BundledLanguage } from "shiki/bundle/web";
 import CopyToClipboard from "./CopyToClipboard";
 
@@ -30,7 +31,12 @@ export default async function CodeBlock({
   return (
     <div className="not-prose overflow-hidden rounded-lg bg-gradient-to-br from-sky-300 to-sky-500 p-4 !pr-0 md:p-8 lg:p-8 [&>pre]:rounded-none">
       <div className="overflow-hidden rounded-s-lg">
-        <div className="flex items-center justify-between bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4">
+        <div
+          className={cn(
+            "flex items-center bg-gradient-to-r from-neutral-900 to-neutral-800 py-2 pl-2 pr-4",
+            filename ? "justify-between" : "justify-end",
+          )}
+        >
           {filename && (
             <span className="-mb-[calc(0.5rem+2px)] rounded-t-lg border-2 border-white/5 border-b-border bg-neutral-800 px-3 py-1 text-sm text-neutral-400">
               {filename}
