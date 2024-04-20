218ad1ac8d6c41b4f09535d424cc367c17efb234
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-04-20 12:49:26 +0200

    style(Tag): make border-radius smaller

--- components/shared/Tag.tsx
+++ components/shared/Tag.tsx
@@ -5,7 +5,7 @@ export default function Tag({ className, children }: Props) {
   return (
     <span
       className={cn(
-        "rounded-lg bg-sky-950 px-2 py-1 text-sm text-sky-400",
+        "rounded bg-sky-950 px-2 py-1 text-sm text-sky-400",
         className,
       )}
     >
