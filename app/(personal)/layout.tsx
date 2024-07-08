a460ced4de2d436ae32aaf0fb962fa77be012770
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2024-07-08 23:28:35 +0200

    refactor: move metadata and viewport config to root layout
    This commit removes the warning related to metadataBase as discussed in https://github.com/vercel/next.js/discussions/57251.

--- app/(personal)/layout.tsx
+++ app/(personal)/layout.tsx
@@ -7,7 +7,6 @@ import { Navbar } from "@/components/global/Navbar";
 import { Toaster } from "@/components/ui/sonner";
 import { tailwindConfig } from "@/tailwind.config";
 import { Analytics } from "@vercel/analytics/react";
-import type { Metadata, Viewport } from "next";
 import dynamic from "next/dynamic";
 import { draftMode } from "next/headers";
 import { Suspense } from "react";
@@ -15,27 +14,6 @@ import { Suspense } from "react";
 const LiveVisualEditing = dynamic(
   () => import("@/sanity/loader/LiveVisualEditing"),
 );
-
-const siteDomain = "https://www.nikolailehbr.ink";
-
-export const metadata: Metadata = {
-  metadataBase: new URL(siteDomain),
-  title: {
-    template: "%s - Nikolai Lehbrink",
-    default: "Nikolai Lehbrink - Web Developer & Designer",
-  },
-  description:
-    "Web enthusiast from Germany. Specializing in the React Ecosystem, dedicated to creating performant, accessible web applications.",
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-  },
-  alternates: { canonical: siteDomain },
-};
-
-export const viewport: Viewport = {
-  themeColor: tailwindConfig.theme.colors.sky["500"],
-};
 
 export default function IndexRoute({
   children,
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import GlobalLayout from "@/components/global/Layout/GlobalLayout";
 import clsx from "clsx";
 import { Blinker } from "next/font/google";
 import "./globals.css";
+import { tailwindConfig } from "@/tailwind.config";
+import type { Metadata, Viewport } from "next";
 
 const blinker = Blinker({
   weight: ["200", "400", "700"],
@@ -10,6 +12,27 @@ const blinker = Blinker({
   display: "swap",
   variable: "--font-blinker",
 });
+
+const siteDomain = "https://www.nikolailehbr.ink";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(siteDomain),
+  title: {
+    template: "%s - Nikolai Lehbrink",
+    default: "Nikolai Lehbrink - Web Developer & Designer",
+  },
+  description:
+    "Web enthusiast from Germany. Specializing in the React Ecosystem, dedicated to creating performant, accessible web applications.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+  },
+  alternates: { canonical: siteDomain },
+};
+
+export const viewport: Viewport = {
+  themeColor: tailwindConfig.theme.colors.sky["500"],
+};
 
 export default function RootLayout({
   children,
