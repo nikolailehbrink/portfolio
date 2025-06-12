afa88a5fc6e9b548fbd6c9c78459085709000173
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-06-12 15:36:38 +0200

    fix: filter routes to resolve duplicated URLs issue in sitemap generation

--- app/routes/sitemap.xml.ts
+++ app/routes/sitemap.xml.ts
@@ -4,13 +4,18 @@ import { href } from "react-router";
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const { routes } = await import("virtual:react-router/server-build");
+  // TODO: Remove this temporary fix, once the issue with duplicated urls is resolved
+  // https://github.com/forge-42/seo-tools/issues/12
+  const filteredRoutes = Object.values(routes).filter(
+    (route) => !(route?.path === undefined && route?.parentId !== "root"),
+  );
   const { origin } = new URL(request.url);
   const sitemap = await generateRemixSitemap({
     domain: origin,
     ignore: [href("/api/chat")],
     // @ts-expect-error Type mismatch, maybe related to a stricter type mentioned in release notes for v.7.0.0
     // https://github.com/forge-42/seo-tools/issues/8
-    routes,
+    routes: filteredRoutes,
   });
 
   return new Response(sitemap, {
