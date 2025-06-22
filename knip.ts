7bab40e79c4ddd9d72dc0b0aa73a88ac0aba95db
Nikolai Lehbrink <nikolailehbrink@outlook.com>
2025-06-22 11:08:30 +0200

    fix: make knip config work
    - Update css compiler to replace `@plugin` with `@import` for correct handling of `@plugin`
    - Exclude `dotenv` from binaries

--- knip.ts
+++ knip.ts
@@ -1,6 +1,10 @@
 export default {
   compilers: {
-    css: (text: string) =>
-      [...text.matchAll(/(?<=@)(import|plugin)[^;]+/g)].join("\n"),
+    css: (text: string) => {
+      // https://github.com/webpro-nl/knip/issues/1008#issuecomment-2756572278
+      text = text.replace("@plugin", "@import");
+      return [...text.matchAll(/(?<=@)import[^;]+/g)].join("\n");
+    },
   },
+  ignoreBinaries: ["dotenv"],
 };
