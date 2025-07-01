19f7ff431aec50238b2d44884f11214f489ea847
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-07-01 18:45:32 +0200

    fix: update prerender paths to use href and add sitemap post

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,15 +1,17 @@
 import { vercelPreset } from "@vercel/react-router/vite";
 import type { Config } from "@react-router/dev/config";
+import { href } from "react-router";
 
 export default {
   // In order to make the local preview work, because the preset changes the server index path
   ...(process.env.VERCEL ? { presets: [vercelPreset()] } : {}),
   prerender: [
-    "/privacy-policy",
-    "/legal-notice",
-    "/blog/batch-mails-deno-postmark",
-    "/blog/tailwind-css-tips",
-    "/blog/syntax-highlighting-shiki-next-js",
-    "/blog/fonts-remix-react-router-7",
+    href("/blog/batch-mails-deno-postmark"),
+    href("/blog/fonts-remix-react-router-7"),
+    href("/blog/sitemap-react-router-7"),
+    href("/blog/syntax-highlighting-shiki-next-js"),
+    href("/blog/tailwind-css-tips"),
+    href("/privacy-policy"),
+    href("/legal-notice"),
   ],
 } satisfies Config;
