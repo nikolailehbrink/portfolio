dc3cd8135949048135157e72643fe8046a4770de
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-07-18 18:40:49 +0200

    feat: add UnpicImage component for enhanced image handling

---
+++ app/components/ui/image.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import { Image as UnpicImage, type ImageProps } from "@unpic/react";
+
+export default function Image({
+  caption,
+  className,
+  ...props
+}: ImageProps & { caption?: string }) {
+  const image = (
+    <UnpicImage
+      className={cn(
+        "rounded-lg object-cover",
+        { "saturate-[1.2]": import.meta.env.PROD },
+        className,
+      )}
+      {...props}
+      {...(import.meta.env.PROD ? { cdn: "vercel" } : {})}
+    />
+  );
+
+  return caption ? (
+    <figure>
+      {image}
+      <figcaption className="flex justify-center text-center">
+        {caption}
+      </figcaption>
+    </figure>
+  ) : (
+    image
+  );
+}
