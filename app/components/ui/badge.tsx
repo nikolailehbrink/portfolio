179143035d3e80e509c4bca01f6e60e5cfa43549
Nikolai Lehbrink <mail@nikolailehbr.ink>
2025-08-18 21:13:12 +0200

    style: update components

--- app/components/ui/badge.tsx
+++ app/components/ui/badge.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 const badgeVariants = cva(
   `inline-flex w-fit shrink-0 items-center justify-center gap-1 overflow-hidden
   rounded-md border px-1.5 py-1 text-xs whitespace-nowrap transition-colors
-  focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50
+  focus-visible:border-ring focus-visible:ring-2 focus-visible:ring-ring/50
   aria-invalid:border-destructive aria-invalid:ring-destructive/20
   dark:aria-invalid:ring-destructive/40`,
   {
